3c6ee83f35b1d82164437c1cd3b50b6ec26ee873
Alessandro Volpicella <alessandro.volpicella@gmail.com>
2024-03-09 09:47:08 +0100

    move global.css files to styles folder

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import Link from "next/link";
-import "../../app/globals.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
